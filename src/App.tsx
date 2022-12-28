0f4e9301eb0923dd9005b509cf9cfb1bc1fb4196
mahmoudessam16 <mahmoudemourad29@gmail.com>
2022-12-28 16:51:42 +0200

    Modifing React Router

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ function App() {
             <Navbar />
             <Container className="mb-4">
                 <Routes>
-                    <Route path="/" element={<Home />} />
+                    <Route path="/home" element={<Home />} />
                     <Route path="/store" element={<Store />} />
                     <Route path="/about" element={<About />} />
                 </Routes>
